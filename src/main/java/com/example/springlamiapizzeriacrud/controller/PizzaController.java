ef833bb8aa4d38547432bb09e35a7ea5a4818638
SamueleLettieri <samuele2073@gmail.com>
2023-02-15 16:40:06 +0100

    fine esercizio

--- src/main/java/com/example/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/com/example/springlamiapizzeriacrud/controller/PizzaController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.springlamiapizzeriacrud.model.Pizza;
 import com.example.springlamiapizzeriacrud.repository.PizzaRepository;
@@ -18,9 +20,26 @@ public class PizzaController {
 	private @Autowired PizzaRepository pizzaRepository;
 	
 	@GetMapping
-	public String index(Model model) {
-		List<Pizza> elencoPizze = pizzaRepository.findAll();
+	public String index(@RequestParam(name="keyword", required = false) String keyword, Model model) {
+		List<Pizza> elencoPizze;
+		
+		if (keyword!=null && !keyword.isEmpty()) {
+			elencoPizze = pizzaRepository.findByNameLike("%"+keyword+"%");
+		} else {
+			elencoPizze = pizzaRepository.findAll();
+		}
+
 		model.addAttribute("elencoPizze", elencoPizze);
 		return"pizze/index";
 	}
+	
+	
+		
+	@GetMapping("pizze/{id}")
+	public String detail(@PathVariable("id") Integer id, Model model) {
+		Pizza pizza = pizzaRepository.getReferenceById(id);
+		model.addAttribute("pizza", pizza);
+		return "pizze/detail";
+	}
+
 }
--- src/main/java/com/example/springlamiapizzeriacrud/repository/PizzaRepository.java
+++ src/main/java/com/example/springlamiapizzeriacrud/repository/PizzaRepository.java
@@ -1,9 +1,13 @@
 package com.example.springlamiapizzeriacrud.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.springlamiapizzeriacrud.model.Pizza;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer>{
 
+	public List<Pizza> findByNameLike(String keyword);
+	
 }
